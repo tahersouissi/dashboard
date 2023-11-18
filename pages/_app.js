85852e9b23a5bb5e7b3d887dde2acf1905624649
taher 2 collab <tahirouta12@gmail.com>
2023-11-18 12:49:59 +0100

    taher

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({
   Component, pageProps: { session, ...pageProps }
 }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={ session}>
       <Component {...pageProps}/>
     </SessionProvider>
   )
